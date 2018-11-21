21df1a1e9d9ca344662ea98498e980ec66456f7c
pibodi <efbesre@mail.ru>
2018-11-21 17:36:18 +0300

    Refactoring of code

--- static/scripts/myTypeAheadScript.js
+++ static/scripts/myTypeAheadScript.js
@@ -1,18 +1,15 @@
-var $input = $('.typeahead');
-var $drpdItem
-$input.typeahead({
-  minLength: 2,
-  source: $.get("cities.json", function(data){
-  $("#choiseCityName").typeahead({ source:data,
-    displayText: function (item) {
-    	return item.name;
-    },
-    afterSelect: function(item) {
-      
-      return weather.showCityWeather($input.val())
-    },
-    highlighter: function(item, data) {
-        return `<div> ${item} <strong>${data.country}</strong></div>`
-    }
-   });
-},'json')});
+jQuery(document).ready(function(){
+  $.get("major_cities.json", function(data){
+    $("#choiseCityName").typeahead({ source:data,
+      displayText: function (item) {
+      	return item.name;
+      },
+      afterSelect: function(item) {
+        weather.showCityWeather($("#choiseCityName").val())
+      },
+      highlighter: function(item, data) {
+          return `<div>${item}<strong>${data.country}</strong></div>`
+      },
+    });
+  },'json')
+});
