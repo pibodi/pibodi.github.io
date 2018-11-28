41bf2864f4ae9d6d4a3923c643937221ba35dfec
pibodi <efbesre@mail.ru>
2018-11-28 18:59:10 +0300

    added support scrolling for canvas #myChart

--- static/scripts/myTypeAheadScript.js
+++ static/scripts/myTypeAheadScript.js
@@ -12,11 +12,30 @@ jQuery(document).ready(function(){
       },
     });
   },'json');
-
-
 });
 $('#choiseCityName').keydown(function(e) {
   if (e.keyCode === 13) {
     return weather.showCityWeather($("#choiseCityName").val())
   }
+});
+
+$('#myGeoWeather').on('click', function(){
+   return weather.showWeather();
+})
+//from stackoverflow
+jQuery(function ($) {
+    $.fn.hScroll = function (amount) {
+        amount = amount || 120;
+        $(this).bind("DOMMouseScroll mousewheel", function (event) {
+            var oEvent = event.originalEvent,
+                direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta,
+                position = $(this).scrollLeft();
+            position += direction > 0 ? -amount : amount;
+            $(this).stop().animate({scrollLeft: position}, 270)
+            event.preventDefault();
+        })
+    };
+});
+$(document).ready(function(){
+    $('#chart').hScroll(200);
 });
